0cac080b66df089004cab33b4756bb873a3ead8e
ShinjaeKwon <80821744+ShinjaeKwon@users.noreply.github.com>
2021-06-28 01:46:25 +0900

    JavaPractice
    자바의 정석 Math클래스 및 Wrapper클래스

---
+++ 09java.lang_class/src/MathEx2.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.*;
+public class MathEx2 {
+
+	public static void main(String[] args) {
+		int i = Integer.MIN_VALUE;
+		
+		System.out.println("i ="+i);
+		System.out.println("-i ="+(-i));
+		
+		try{
+			System.out.printf("negateExact(%d) = %d%n", 10, negateExact(10));
+			System.out.printf("negateExact(%d) = %d%n", -10, negateExact(-10));
+			System.out.printf("negateExact(%d) = %d%n", i, negateExact(i)); //예외발생
+		}catch(ArithmeticException e){
+			//i를 long타입으로 형변환다음에 negateExact(long a)를 호출
+			System.out.printf("negateExact(%d) = %d%n", (long)i, negateExact((long)i));
+		}
+	}
+
+}
