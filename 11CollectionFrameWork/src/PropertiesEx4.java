c26c01221e951e01afd91c8332278947b69d5744
ShinjaeKwon <80821744+ShinjaeKwon@users.noreply.github.com>
2021-07-10 16:09:30 +0900

    JavaPractice
    자바의정석11 Properties

---
+++ 11CollectionFrameWork/src/PropertiesEx4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PropertiesEx4 {
+
+	public static void main(String[] args) {
+		Properties sysProperties = System.getProperties();
+		System.out.println("java.version : "+sysProperties.getProperty("java.version"));
+		
+		System.out.println("user.language : "+sysProperties.getProperty("user.language"));
+		
+		sysProperties.list(System.out);
+	}
+
+}
